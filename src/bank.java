7aa73719be96e0820c73df366b2c2668f5220d30
muneesMohammed <marhaba.munees@gmail.com>
2022-07-29 16:24:23 +0530

    code added

---
+++ src/bank.java
@@ -0,0 +1,40 @@
+public class bank {
+    public  void interestrate(){
+        System.out.println("7");
+    }
+}
+class sbi extends bank{
+
+    public  void interestrate(){
+        System.out.println("7.1");
+    }
+
+}
+class icic extends bank{
+
+    public  void interestrate(){
+        System.out.println("7.5");
+    }
+
+}
+class hdfc extends bank{
+
+    public  void interestrate(){
+        System.out.println("7.45");
+    }
+
+}
+class axis extends bank{
+
+    public  void interestrate(){
+        System.out.println("7.5");
+    }
+
+}
+class pnb extends bank{
+
+    public  void interestrate(){
+        System.out.println("7.1");
+    }
+
+}
---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main extends bank {
+    public static void main(String[] args) {
+        bank intr = new bank();
+        intr.interestrate();
+        bank intr2 = new sbi();
+        intr2.interestrate();
+        bank intr3 = new icic();
+        intr3.interestrate();
+        bank intr4 = new hdfc();
+        intr4.interestrate();
+        bank intr5 = new axis();
+        intr5.interestrate();
+        bank intr6 = new pnb();
+        intr6.interestrate();
+
+    }
+}
